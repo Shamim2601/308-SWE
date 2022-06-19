c2e420ed482964bcc69ae2104636459bc7d39ebb
Shamim2601 <shamim2601@gmail.com>
2022-06-19 13:06:20 +0600

    prb 1 done

--- OFFLINE_3/AdapterPackage/adapter.java
+++ OFFLINE_3/AdapterPackage/adapter.java
@@ -1,5 +1,36 @@
 package AdapterPackage;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import ServicePackage.service;
+
 public class adapter {
-    
+    public adapter(){
+
+    }
+
+    public int CalculateSum(File file) throws IOException{
+        try (Scanner scanner = new Scanner(file)) {
+            service serv = new service();
+            String str = "";
+            File temp = File.createTempFile("tmp", ".txt");
+            FileWriter fw = new FileWriter(temp);
+            try (BufferedWriter bWriter = new BufferedWriter(fw)) {
+                while(scanner.hasNext()){
+                    str+= scanner.nextLine();
+                }
+
+                String[] strArray = str.split("~");
+                for(String s: strArray){
+                    bWriter.write(s+" ");
+                }
+            }
+
+            return serv.CalculateSum(temp);
+        }
+    }
 }
--- OFFLINE_3/main.java
+++ OFFLINE_3/main.java
@@ -1,13 +1,19 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import AdapterPackage.adapter;
 import ServicePackage.service;
 
 public class main {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("InputFiles/space_input.txt");
+    public static void main(String[] args) throws IOException {
+        //Calculating sum using the service class
+        File file1 = new File("InputFiles/space_input.txt");
         service service1 = new service();
+        System.out.println("\nSum using service: "+ service1.CalculateSum(file1));
 
-        System.out.println(service1.CalculateSum(file));
+        //Calculating sum using the adapter class
+        File file2 = new File("InputFiles/notted_input.txt");
+        adapter adapter1 = new adapter();
+        System.out.println("\nSum using adapter: "+ adapter1.CalculateSum(file2));
     }
 }
